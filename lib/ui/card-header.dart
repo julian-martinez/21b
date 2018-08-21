799ef9dcf57f3e05a7419ecdbe2774a18661f38c
Julián Martínez <julian@MacBook-Pro-de-Julian.local>
2018-08-21 10:27:14 +0200

    card-header opacity

--- lib/ui/card-header.dart
+++ lib/ui/card-header.dart
@@ -14,56 +14,57 @@ class _CardHeaderState extends State<CardHeader> {
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      decoration: new BoxDecoration(
-        color: Colors.white
-      ),
-      child: new Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new DrawCircle.small(
-                color: Colors.red,
-                alignment: Alignment.centerRight,
-              ),
-              new Container(
-                padding: const EdgeInsets.only(left: 4.0, right: 4.0),
-                child: new Text(widget.nick, textAlign: TextAlign.center,),
-              ),
-              new DrawCircle.small(
-                color: Colors.red,
-                alignment: Alignment.centerLeft,
-              ),
-            ],
-          ),
-
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              new Container(
-                child: new Text(
-                    calculateDateTimeDifference(widget.dateTime)
+    return new Opacity(
+      opacity: 0.9,
+      child: new Container(
+        decoration: new BoxDecoration(
+            color: Colors.white
+        ),
+        child: new Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new DrawCircle.small(
+                  color: Colors.red,
+                  alignment: Alignment.centerRight,
                 ),
-              ),
-              new Container(
-                child: new PopupMenuButton<String>(
-                    itemBuilder: (BuildContext context){
-                      return Constants.choices.map((String choice){
-                        return PopupMenuItem<String>(
-                          value: choice,
-                          child: new Text(choice),
-                        );
-                      }).toList();
-                    }
+                new Container(
+                  padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                  child: new Text(widget.nick, textAlign: TextAlign.center,),
                 ),
-              ),
-            ],
-          ),
-
-        ],
-      ),
+                new DrawCircle.small(
+                  color: Colors.red,
+                  alignment: Alignment.centerLeft,
+                ),
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                new Container(
+                  child: new Text(
+                      calculateDateTimeDifference(widget.dateTime)
+                  ),
+                ),
+                new Container(
+                  child: new PopupMenuButton<String>(
+                      itemBuilder: (BuildContext context){
+                        return Constants.choices.map((String choice){
+                          return PopupMenuItem<String>(
+                            value: choice,
+                            child: new Text(choice),
+                          );
+                        }).toList();
+                      }
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 
