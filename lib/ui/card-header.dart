e6198bfd0f5db8d5d2f0c9fee0173b960db6059e
Julián Martínez <julian@MacBook-Pro-de-Julian.local>
2018-08-21 10:59:57 +0200

    circle color

--- lib/ui/card-header.dart
+++ lib/ui/card-header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class CardHeader extends StatefulWidget {
   CardHeader({Key key, @required this.nick, @required this.dateTime}) : super(key: key);
@@ -14,6 +15,8 @@ class _CardHeaderState extends State<CardHeader> {
 
   @override
   Widget build(BuildContext context) {
+    Color _color = getRandomColor();
+
     return new Opacity(
       opacity: 0.9,
       child: new Container(
@@ -27,7 +30,7 @@ class _CardHeaderState extends State<CardHeader> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new DrawCircle.small(
-                  color: Colors.red,
+                  color: _color,
                   alignment: Alignment.centerRight,
                 ),
                 new Container(
@@ -35,7 +38,7 @@ class _CardHeaderState extends State<CardHeader> {
                   child: new Text(widget.nick, textAlign: TextAlign.center,),
                 ),
                 new DrawCircle.small(
-                  color: Colors.red,
+                  color: _color,
                   alignment: Alignment.centerLeft,
                 ),
               ],
@@ -51,7 +54,7 @@ class _CardHeaderState extends State<CardHeader> {
                 new Container(
                   child: new PopupMenuButton<String>(
                       itemBuilder: (BuildContext context){
-                        return Constants.choices.map((String choice){
+                        return ConstActions.choices.map((String choice){
                           return PopupMenuItem<String>(
                             value: choice,
                             child: new Text(choice),
@@ -66,6 +69,12 @@ class _CardHeaderState extends State<CardHeader> {
         ),
       )
     );
+
+  }
+
+  Color getRandomColor(){
+    final _random = new Random();
+    return ConstColors.choices[_random.nextInt(ConstColors.choices.length)];
   }
 
   String calculateDateTimeDifference(DateTime origin){
@@ -112,7 +121,24 @@ class DrawCircle extends StatelessWidget {
 }
 
 
-class Constants{
+class ConstColors{
+  static const Color olive = Color.fromRGBO(154, 155, 118, 1.0);
+  static const Color darkbeige = Color.fromRGBO(174, 156, 143, 1.0);
+  static const Color beige = Color.fromRGBO(229, 216, 205, 1.0);
+  static const Color ocre = Color.fromRGBO(236, 155, 49, 1.0);
+  static const Color salmon = Color.fromRGBO(198, 116, 100, 1.0);
+  static const Color blue = Color.fromRGBO(111, 137, 151, 1.0);
+  static const Color darkblue = Color.fromRGBO(68, 83, 92, 1.0);
+  static const Color red = Color.fromRGBO(165, 74, 83, 1.0);
+  static const Color brown = Color.fromRGBO(91, 66, 61, 1.0);
+
+  static const List<Color> choices = <Color>[
+    olive, darkbeige, beige, ocre, salmon, blue, darkblue, red, brown
+  ];
+}
+
+
+class ConstActions{
   static const String activate = 'Activar notificaciones de Post';
   static const String report = 'Reportar post';
 
